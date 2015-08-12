aa90784bf67b6421de407d56f90f06d651a11ae6
bogdan <perldev@mail.ru>
2015-08-13 02:35:28 +0300

    tab to spaces

--- main/management/commands/fill_bins.py
+++ main/management/commands/fill_bins.py
@@ -13,27 +13,27 @@ class Command(BaseCommand):
     help = 'fix user currency'
     def handle(self, *args, **options):
 
-            Now = datetime.now()
-            for item in get_bins():
-		Bin = item["bin"]
-		print "process bin %s" % Bin  
-                try:
-                    Bin =  FullBinInfo.objects.get(bin6 = Bin)
-                    continue            
-                                
-                except FullBinInfo.DoesNotExist:
-                    pass
-		print "add bin %s" % item["bin"]
-                Bin = FullBinInfo(bin6 = item["bin"],
-                                      country = item["country"],
-                                      product = item["product"],
-                                      bank = item["bank"],
-                                      prepaid= item["prepaid"],
-                                      pbbin = item["pbbin"],
-                                      alphacode = item["alphacode"] 
-                                      )
-                Bin.save()
-                
+        Now = datetime.now()
+        for item in get_bins():
+            Bin = item["bin"]
+            print "process bin %s" % Bin  
+            try:
+                Bin =  FullBinInfo.objects.get(bin6 = Bin)
+                continue            
+                            
+            except FullBinInfo.DoesNotExist:
+                pass
+            print "add bin %s" % item["bin"]
+            Bin = FullBinInfo(bin6 = item["bin"],
+                                country = item["country"],
+                                product = item["product"],
+                                bank = item["bank"],
+                                prepaid= item["prepaid"],
+                                pbbin = item["pbbin"],
+                                alphacode = item["alphacode"] 
+                                )
+            Bin.save()
+            
   
 def get_bins( ):
     Url = "https://ecommerce.liqpay.com/ecommerce/fullbininfo2"
--- main/management/commands/generate_pool_wallets.py
+++ main/management/commands/generate_pool_wallets.py
@@ -10,27 +10,27 @@ class Command(BaseCommand):
     args = ''
     help = 'adding lots of accounts'
     def handle(self, *args, **options):
-         for   CurrencyTitle  in ('LTC', 'DOGE', 'NVC', 'DRK', 'RMS', 'CLR', 'PPC'):
+        for   CurrencyTitle  in ('LTC', 'DOGE', 'NVC', 'DRK', 'RMS', 'CLR', 'PPC'):
             Crypton = CryptoAccount(CurrencyTitle, "trade_stock")
-	    CurIns = Currency.objects.get(title = CurrencyTitle)
+            CurIns = Currency.objects.get(title = CurrencyTitle)
         
-	    bulk_add = []
-  	    Addresses = {}
-                   
-	    for i in xrange(1,50):
-                 FreeAccount = PoolAccounts(currency = CurIns, status = "created")                      
-                 FreeAccount.pub_date = date.today() 
-                 NewAdress  = Crypton.getnewaddress()
-                 FreeAccount.address = NewAdress 
-			
-		 if Addresses.has_key(NewAdress):
-			print "repeat address %s" % (NewAdress)
-		 else:
-		 	print "generate %i %s" % (i, NewAdress)
-                 	bulk_add.append(FreeAccount)    
-                 
+            bulk_add = []
+            Addresses = {}
+                
+            for i in xrange(1,50):
+                    FreeAccount = PoolAccounts(currency = CurIns, status = "created")                      
+                    FreeAccount.pub_date = date.today() 
+                    NewAdress  = Crypton.getnewaddress()
+                    FreeAccount.address = NewAdress 
+            
+                    if Addresses.has_key(NewAdress):
+                        print "repeat address %s" % (NewAdress)
+                    else:
+                        print "generate %i %s" % (i, NewAdress)
+                        bulk_add.append(FreeAccount)    
+                
             PoolAccounts.objects.bulk_create(bulk_add)
-                 
+                
                                 
            
                                 
