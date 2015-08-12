b5e9a48882ad61dd9dc375a43f04ddaf8aff208f
bogdan <perldev@mail.ru>
2015-08-13 00:49:19 +0300

    restart with encrypting settings

--- restart_api.sh
+++ restart_api.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-kill `cat /home/btctrade/stock/manage_api.pid`
-python ./manage.py runfcgi host=127.0.0.1 port=8367 daemonize=true maxspare=2 pidfile=/home/btctrade/stock/manage_api.pid
+kill `cat manage_api.pid`
+openssl des3 -d -in settings2.des3 -out crypton/settings.py
+#uwsgi --ini uwsgi_api.ini
+python ./manage.py runfcgi host=0.0.0.0 port=8367 daemonize=true maxspare=14 pidfile=/home/btctrade/stock/manage_api.pid
+rm crypton/settings.py
