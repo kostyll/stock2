a4f89e3a766cf815a7db29f5e5b00cc7f91d5072
bogdan <perldev@mail.ru>
2015-08-13 14:57:55 +0300

    adding debug logging to tornado api

--- main/management/commands/tornado_api.py
+++ main/management/commands/tornado_api.py
@@ -16,8 +16,24 @@ class Command(BaseCommand):
     help = 'start api tornado server'
 
     def handle(self, *args, **options):
+          try:
+            debug = args[0]
+          except:
+            setup_logging()  
+            
           worker = TornadoServer.create_instance(8081, application_urls)
-    
-
           worker.start()
+          
+def setup_logging():
+        logger = logging.getLogger()
+        # remove all handlers
+        logger.handlers = []
+        logger.setLevel(logging.DEBUG)
+        filename = "tornado_api.log" 
+        ch = logging.FileHandler(filename, mode='a')
+        logging.Formatter('%(asctime)s [%(levelname)s] %(module)s.%(funcName)s: %(message)s')
+        ch.setLevel(logging.DEBUG)
+        logger.addHandler(ch)
+
+    
 
