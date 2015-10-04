442cc69bfebe1b62186bc3c7e56168b0de8ccbde
bogdan <perldev@mail.ru>
2015-10-04 23:35:54 +0300

    Update restart_api.sh

--- restart_api.sh
+++ restart_api.sh
@@ -2,5 +2,5 @@
 kill `cat manage_api.pid`
 #openssl des3 -d -in settings2.des3 -out crypton/settings.py
 #uwsgi --ini uwsgi_api.ini
-python ./manage.py runfcgi host=0.0.0.0 port=8367 daemonize=true maxspare=4 pidfile=/home/btctrade/stock/manage_api.pid
+python ./manage.py runfcgi host=0.0.0.0 port=8367 daemonize=true maxspare=4 pidfile=/home/btc_trade/stock2/manage_api.pid
 #rm crypton/settings.py
