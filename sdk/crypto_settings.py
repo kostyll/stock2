0430e135a81d410ea0f9cde20b5a998ecf305a18
bogdan <perldev@mail.ru>
2015-10-19 01:52:31 +0300

    Create crypto_settings.py

---
+++ sdk/crypto_settings.py
@@ -0,0 +1,36 @@
+Settings ={
+       "BTC":{
+               "host":"0267f3165-d2d5a2dcd4e1",
+                "port":"90",
+                "rpc_user":"Odedirty",
+                "rpc_pwd":"Ria.45",
+                "user":"trade_stock",
+                "min_confirmation":3
+
+                },
+        "LTC":{
+                "host":"10.8.0.1",
+                "port":"932",
+                "rpc_user":"rpc_itecoin_asda",
+                "rpc_pwd":"sharsxccccccccccccaaweqwdf",
+                "user":"trade_stock",
+                "min_confirmation":6
+                 },
+        "NVC":{
+                "host":"10.8.0.1",
+                "port":"93",
+                "rpc_user":"mycoinrpc",
+                "rpc_pwd":"JQ_asda345343cxvsdFDS_a",
+                "user":"trade_stock",
+                 "min_confirmation":6
+        },
+        "DRK":{
+
+                "host":"10.8.0.1",
+                "port":"900",
+                "rpc_user":"darkrpc",
+                "rpc_pwd":"darkoin_pasda_ubuntui48",
+                "user":"trade_stock",
+                 "min_confirmation":6
+}
+}
